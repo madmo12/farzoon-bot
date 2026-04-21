ae8677a42fd75fda586576a57b4a93afcb50f999
Moaz Mohamed <madmo0978@gmail.com>
2026-04-22 01:08:39 +0200

    switch to groq

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const groq = new OpenAI({
 async function askAI(question) {
   try {
     const completion = await groq.chat.completions.create({
-      model: "mixtral-8x7b-32768",
+      model: "llama-3.1-8b-instant",
       messages: [
         {
           role: "system",
